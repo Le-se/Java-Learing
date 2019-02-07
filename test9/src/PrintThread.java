93d78e3330a850681bc92ea61d9d8bac994ec07a
Le-se <691681966@qq.com>
2019-02-07 20:01:39 +0800

    提交文件

--- test9/src/PrintThread.java
+++ test9/src/PrintThread.java
@@ -1,2 +1,7 @@
-public class PrintThread {
+public class PrintThread extends Thread {
+    public void run() {
+        for(int i=0;i<5;i++){
+            System.out.println(currentThread().getName()+"正在运行");
+        }
+    }
 }
--- test9/src/TestThread.java
+++ test9/src/TestThread.java
@@ -1,2 +1,12 @@
 public class TestThread {
+    public static void main(String[] args) {
+        PrintThread pt1=new PrintThread();
+        PrintThread pt2=new PrintThread();
+        Thread pt1s=new Thread(pt1);
+        Thread pt2s=new Thread(pt2);
+        pt1s.setName("线程1");
+        pt2s.setName("线程2");
+        pt1s.start();
+        pt2s.start();
+    }
 }
